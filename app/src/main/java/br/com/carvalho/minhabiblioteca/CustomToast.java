246672a006bd6648f32603b75e50b2c198d77318
logonrm <logonrm@fiap.com.br>
2018-03-26 21:00:28 -0300

    Initial commit

---
+++ app/src/main/java/br/com/carvalho/minhabiblioteca/CustomToast.java
@@ -0,0 +1,32 @@
+package br.com.carvalho.minhabiblioteca;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * Created by logonrm on 26/03/2018.
+ */
+
+public class CustomToast {
+
+    public void showToast(Context context, String mensagem) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View customToastroot = inflater.inflate(R.layout.custom_toast, null);
+
+        Toast customtoast = new Toast(context);
+
+        customtoast.setView(customToastroot);
+
+        TextView textView = (TextView) customToastroot.findViewById(R.id.textView1);
+        textView.setText(mensagem);
+        customtoast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0,0 );
+        customtoast.setDuration(Toast.LENGTH_LONG);
+        customtoast.show();
+    }
+}
